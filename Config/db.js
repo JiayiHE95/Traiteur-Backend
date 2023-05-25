669466eecb21efa2228f6f2a3917f0e5cc95c56b
Jiayi HE <he_jia_yi1995@126.com>
2023-05-25 23:38:01 +0200

    cors

--- Config/db.js
+++ Config/db.js
@@ -6,19 +6,20 @@ const login=process.env.LOGIN
 const host=process.env.HOST
 const port=process.env.PORT
 
-/*
-const sequelize = new Sequelize("ordersystem",null,null, {
-  host:"172.17.0.43",
+const sequelize = new Sequelize("ordersystem",'mysql', '753ed129c298e00e', {
+  host:"localhost",
   dialect: "mysql",
   //logging: false
 });
-*/
+
+/*
 const sequelize = new Sequelize("ordersystem", "hejAdmin", "123", {
   host: "127.0.0.1",
   port:3306,
   dialect: "mysql",
   //logging: false
 });
+*/
 
 
 
--- index.js
+++ index.js
@@ -18,6 +18,7 @@ var corsOptions = {
 app.use(cors(corsOptions))
 
 app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     "Access-Control-Allow-Headers",
     "x-access-token, Origin, Content-Type, Accept"
