18c9ffda6c562930e4905fbb7702c16f2948be79
Jiayi HE <he_jia_yi1995@126.com>
2023-05-25 18:27:43 +0200

    variable env

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ var corsOptions = {
   //origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-app.use(cors(corsOptions))
+
+//app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 
 app.use("/user", userRouter);
