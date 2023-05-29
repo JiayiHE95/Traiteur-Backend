2b30e6b0baf0a2ebd0cd67adeea4624a00ddd4c7
Jiayi HE <he_jia_yi1995@126.com>
2023-05-30 01:08:41 +0200

    delete console.log

--- Config/db.js
+++ Config/db.js
@@ -6,19 +6,19 @@ const login=process.env.LOGIN
 const host=process.env.HOST
 const port=process.env.PORT
 
-/*
 const sequelize = new Sequelize("ordersystem",'mysql', '753ed129c298e00e', {
   host:"dokku-mysql-ordersystem",
   port:3306,
   dialect: "mysql",
 });
-*/
 
+/*
 const sequelize = new Sequelize("ordersystem", "hejAdmin", "123", {
   host: "127.0.0.1",
   port:3306,
   dialect: "mysql",
 });
+*/
 
 
 
